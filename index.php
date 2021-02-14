4c3e24fd9b9fe01c05d153444f15decd13a9d7e4
tokuyama shouta <toku1031ts@gmail.com>
2021-02-14 21:03:18 +0900

    パーツファイルに分けるget_header/get_footer

--- index.php
+++ index.php
@@ -3,25 +3,7 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Clean Blog - Start Bootstrap Theme</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="http://mysite.local/wp-content/themes/myblog/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="http://mysite.local/wp-content/themes/myblog/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-  <!-- Custom styles for this template -->
-  <link href="http://mysite.local/wp-content/themes/myblog/css/clean-blog.min.css" rel="stylesheet">
-
-  <?php wp_head(); ?>
+  <?php get_header(); ?>
 </head>
 
 <body>
@@ -139,15 +121,7 @@
       </div>
     </div>
   </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
-
-  <?php wp_footer(); ?>
+  <?php get_footer(); ?>
 </body>
 
 </html>
