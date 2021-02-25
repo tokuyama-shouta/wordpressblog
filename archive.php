49cc65e24a6d08bd56c626e1ee94ea693cc34536
tokuyama shouta <toku1031ts@gmail.com>
2021-02-25 22:45:28 +0900

    カテゴリー作成

--- archive.php
+++ archive.php
@@ -13,8 +13,8 @@
             <div class="row">
               <div class="col-lg-8 col-md-10 mx-auto">
                 <div class="site-heading">
-                  <h1>Clean Blog<?php echo date('Y'); ?></h1>
-                  <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                  <h1>Categoly</h1>
+                  <span class="subheading"><?php wp_title(''); ?></span>
                 </div>
               </div>
             </div>
