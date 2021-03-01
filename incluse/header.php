d05d5ebb05d3eab5b77bbeddb8550d363c2c7300
tokuyama shouta <toku1031ts@gmail.com>
2021-03-01 21:32:04 +0900

    メニュー実装

--- incluse/header.php
+++ incluse/header.php
@@ -6,20 +6,21 @@
         Menu
         <i class="fas fa-bars"></i>
       </button>
+      <?php
+        //メニューIDを取得する
+        $menu_name = 'global_nav';
+        $locations = get_nav_menu_locations();
+        $menu = wp_get_nav_menu_object($locations[$menu_name]);
+        
+        $menu_items = wp_get_nav_menu_items($menu ->term_id);
+      ?>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
+          <?php foreach ($menu_items as $item) : ?>
           <li class="nav-item">
-            <a class="nav-link" href="index.html">ホーム</a>
+            <a class="nav-link" href="<?php echo $item->url?>"><?php echo $item->title; ?></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="post.html">Sample Post</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li>
+          <?php endforeach; ?>
         </ul>
       </div>
     </div>
