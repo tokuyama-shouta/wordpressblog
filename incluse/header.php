caf37cb7de3f21a71513a56949fa962b7064ca86
tokuyama shouta <toku1031ts@gmail.com>
2021-03-02 21:39:44 +0900

    blog仕上げ

--- incluse/header.php
+++ incluse/header.php
@@ -1,7 +1,7 @@
 <?php wp_body_open(); ?>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name')?></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
--- index.php
+++ index.php
@@ -13,8 +13,8 @@
             <div class="row">
               <div class="col-lg-8 col-md-10 mx-auto">
                 <div class="site-heading">
-                  <h1>Clean Blog<?php echo date('Y'); ?></h1>
-                  <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                  <h1><?php bloginfo('name'); ?></h1>
+                  <span class="subheading"><?php bloginfo('description'); ?></span>
                 </div>
               </div>
             </div>
